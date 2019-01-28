d610bfa46827b3fb90900690956f648c2891ef9b
Sim Greenbaum <gbs4ever2@gmail.com>
2019-01-28 22:04:55 +0000

    Done.

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,21 @@
+class  MP3Importer
+ attr_reader :path
+def initialize(path)
+  @path = path
+end
+
+  def files
+      @files ||Dir.chdir(@path) do |path|
+    Dir.glob('*.mp3')
+    end
+  end
+    def import
+      files.each do |file|
+      Song.new_by_filename(file)
+    end
+  end
+
+
+
+
+end
