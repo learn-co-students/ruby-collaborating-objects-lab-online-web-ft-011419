0ada5f280286bd659bcfaa3b0e3ff3129e89dc25
Michael J. Schiumo <mschiumo18@gmail.com>
2019-01-30 17:06:28 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,15 +10,17 @@ class Song
   end
   
   def self.new_by_filename(filename)
-    song = self.new
     song.title = filename.split(" - ")[1]
+    song.artist = filename.split(" -  ")[0]
     song
   end  
   
   def artist
     song.artist = artist
-    
   end  
-    
+  
+  def artist_name=(name)
+    artist_name = artist.name
+  end  
   
 end  
