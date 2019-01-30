5aa731e1d17b14127e78514af195a90aafeb20ea
Michael J. Schiumo <mschiumo18@gmail.com>
2019-01-30 16:03:28 +0000

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -6,16 +6,15 @@ class MP3Importer
     @path = path
   end  
   
-  def files 
-    Dir.glob(path)
+  def files(files) 
+    Dir.glob('./spec/fixtures/mp3s')
+    parts = files.split(" - ")
+    artist = parts[0]
+    song_name = parts[1].gsub(".mp3", "")
   end  
   
   def import(list_of_filenames)
-    list_of_filenames.each {|filename| Song.new_by_filename(filename) }
-    parts = list_of_filenames.split(" - ")
-    artist = parts[0]
-    song_name = parts[1].gsub(".mp3", "")
-    
+    list_of_filenames.each {|filename| Song.new_by_filename(filename)} 
   end
   
   
